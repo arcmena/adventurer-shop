5b65893bdee8b01fb2562b034b0934002384b468
Marcelo Mena <marcelomena010@gmail.com>
2020-08-13 20:49:15 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import items from './assets/store/items.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Adventurer Shop!</h1>
+      {items.map((item) => (
+        <div className="item" key={item.id}>
+          <img src={item.image} alt="Item Image" />
+          <span>{item.name}</span>
+        </div>
+      ))}
     </div>
   );
 }
